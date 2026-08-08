0b715b83b2ec17b8fa242027ff479cf341e8f550
kunal kumar <kk6547015@gmail.com>
2026-08-08 15:04:45 +0530

    fix: revert to Prisma v6 to use standard connection string without adapters

--- Backend/src/config/prisma.js
+++ Backend/src/config/prisma.js
@@ -1,11 +1,5 @@
 const { PrismaClient } = require("@prisma/client");
-const { PrismaMariaDb } = require("@prisma/adapter-mariadb");
-const mariadb = require("mariadb");
 
-const url = process.env.DATABASE_URL ? process.env.DATABASE_URL.replace("mysql://", "mariadb://") : "";
-const pool = mariadb.createPool(url);
-const adapter = new PrismaMariaDb(pool);
-
-const prisma = new PrismaClient({ adapter });
+const prisma = new PrismaClient();
 
 module.exports = prisma;
